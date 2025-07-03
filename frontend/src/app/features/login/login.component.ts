31921494c3062a83891505df76370df06e9ae23f
Yaryna Laiter <91200119+Yasya12@users.noreply.github.com>
2025-07-03 18:21:34 +0300

    Add toaster if you can't log in

--- frontend/src/app/features/login/login.component.ts
+++ frontend/src/app/features/login/login.component.ts
@@ -4,6 +4,7 @@ import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../core/services/auth.service';
 import { LoginRequest } from '../../models/login-request.model';
+import { Toast, ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -13,16 +14,20 @@ import { LoginRequest } from '../../models/login-request.model';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-
+  //services
   private fb = inject(FormBuilder);
   private authService = inject(AuthService);
   private router = inject(Router);
+  private toastr = inject(ToastrService);
 
+  //states
   loginForm = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required]]
   });
+  errorMessage: string | null = null;
 
+  //methods
   onSubmit() {
     if (this.loginForm.invalid) {
       return;
@@ -34,8 +39,14 @@ export class LoginComponent {
     };
 
     this.authService.login(loginData).subscribe({
-      next: () => this.router.navigate(['/']),
-      error: (err) => console.error('Login failed', err)
+      next: () => {
+        this.errorMessage = null;
+        this.router.navigate(['/']);
+      },
+      error: (err) => {
+        this.toastr.error('Login failed. Please try again.')
+        console.error('Login failed', err);
+      }
     });
   }
 }
